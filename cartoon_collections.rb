e962c9dcac66bb30b607b76bac577451f0738133
Alex Hare <alexandra.f.hare@gmail.com>
2019-04-08 21:40:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,39 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves (array)
+  array.each_with_index do |index, name|
+    puts "#{index+1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet (array)
+  new_array = []
+  array.collect do |w|
+  w[0].upcase
+  new_array << w
+  end
+  new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls (array)
+  array.each do |w|
+    counter = 0 
+    if w.length > 4
+      counter ++
+      if counter > 0 
+        return true
+      else 
+        return false
+      end
+    end
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese (array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese|
+    if array.include?(cheese)
+      return cheese 
+      else
+      return NIL
+    end
+  end
 end
